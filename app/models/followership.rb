8400ec4060a9de565e40c2edefea9f3e0aea3a30
jessiewuwu & OaklandRocks & Ryan Loughlin & vibrantlife <pair+jessiewuwu.oaklandrocks.ryanloughlin25.vibrantlife@devbootcamp.com>
2015-02-26 15:50:09 -0800

    add relations to user model to get followees

--- app/models/followership.rb
+++ app/models/followership.rb
@@ -1,5 +1,5 @@
 class Followership < ActiveRecord::Base
-  belongs_to :user, :foreign_key =>"user_id", :class_name =>"User"
+  belongs_to :user
   belongs_to :follower, :foreign_key =>"follower_id", :class_name =>"User"
-
+  belongs_to :followee, :foreign_key =>"user_id", :class_name =>"User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ActiveRecord::Base
   include BCrypt
   has_many :tweets
   has_many :followerships, :foreign_key =>"user_id", :class_name => "Followership"
-  has_many :followers, through: :followerships
+  has_many :followers, through: :followerships, :source => "follower"
+
+  has_many :followeeships, :foreign_key =>"follower_id", :class_name => "Followership"
+  has_many :followees, through: :followeeships, :source => "followee"
 
 
   def password
