a817f4492c774c277d73d56250bb68598c313c63
jessiewuwu & OaklandRocks & Ryan Loughlin & vibrantlife <pair+jessiewuwu.oaklandrocks.ryanloughlin25.vibrantlife@devbootcamp.com>
2015-02-26 13:01:17 -0800

    fix typos in followership model and migration

--- app/models/followership.rb
+++ app/models/followership.rb
@@ -1,5 +1,5 @@
 class Followership < ActiveRecord::Base
   belongs_to :user, :foreign_key =>"user_id", :class_name =>"User"
-  belongs_to :foller, :foreign_key =>"follwer_id", :class_name =>"User"
+  belongs_to :follower, :foreign_key =>"follower_id", :class_name =>"User"
 
 end
--- db/migrate/20150226120253_create_followerships.rb
+++ db/migrate/20150226120253_create_followerships.rb
@@ -1,8 +1,9 @@
-class CreateFollowership < ActiveRecord::Migration
+class CreateFollowerships < ActiveRecord::Migration
   def change
     create_table :followerships do |t|
       t.references :user, index: true
       t.references :follower, index: true
       t.timestamps
+    end
   end
 end
