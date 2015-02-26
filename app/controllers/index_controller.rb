3bd4ed7d9b0b872f6795c7e17dd4ba7630054232
OaklandRocks & vibrantlife <pair+oaklandrocks.vibrantlife@devbootcamp.com>
2015-02-26 14:00:54 -0800

    signup view and routes with session user_id

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,8 +1,26 @@
-
 get '/' do
   erb :index
 end
 
+#logging in
 post '/' do
   @user = User.find(params[:user])
+end
+
+
+
+#--- sign-up ---#
+get '/signup' do
+  erb :signup
+end
+
+post '/signup' do
+  user = User.create(params[:user])
+
+  if user.save
+    session[:user_id] = user.id
+    redirect ("/")
+  else
+    redirect '/signup'
+  end
 end
