d8aa4ba1588d0d15d6d64448c72dbf0177733e8f
OaklandRocks & Ryan Loughlin <pair+oaklandrocks.ryanloughlin25@devbootcamp.com>
2015-02-27 16:55:51 -0800

    seed tweets

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,15 @@ def seed_user(count)
   count.times {User.create(username: Faker::Name.name, email: Faker::Internet.email, password: 'twitty')}
 end
 
+def seed_tweets(count, id)
+  count.times {Tweet.create(content: Faker::Lorem.words(4).join(" ") + "?", user_id: id)}
+end
+
 seed_user(10)
-# def seed_tweets(count, id)
-#   count.times {Question.create(title: Faker::Lorem.words(4).join(" ") + "?", survey_id: id)}
-# end
+seed_tweets(5, 1)
+seed_tweets(2, 2)
+seed_tweets(7, 3)
+seed_tweets(8, 4)
 
 # def seed_all
 #   seed_user(10)
